43bb42a9c96e8642a60f9222620facc28b1e1818
TheClassyPenguin <salvador19marti@gmail.com>
2020-09-22 02:15:10 +0200

    Made all the things

--- R/linreg.R
+++ R/linreg.R
@@ -5,12 +5,14 @@ Linreg <- setRefClass("Linreg",
                                      formula = "formula",
                                      data = "data.frame",
                                      pvalues = "vector",
+                                     tvalues = "vector",
                                      coefficients = "matrix",
                                      call_params = "character",
                                      residuals = "matrix",
                                      fitted = "matrix",
-                                     resvar = "vector"
-                                     
+                                     resvar = "vector",
+                                     outliers = "vector",
+                                     degrees_of_freedom = "numeric"
                                      ),
                       
                       methods = list(
@@ -39,24 +41,72 @@ Linreg <- setRefClass("Linreg",
                           residuals <<- e
 
                           df = nrow(X) - ncol(X)
+                          degrees_of_freedom <<- df
                           
                           s_hat = as.vector((t(e)%*%e)/df)                      #residual variance
-                          #print(class(s_hat))
                           resvar <<- s_hat
                           
                           var_beta_hat = s_hat * solve(t(X) %*% X)              #variance of regression coefficients
                           #print(var_beta_hat)
                           t_val = as.vector(beta_hat) / sqrt(pmax(0,var_beta_hat))                   #t-values
+                          tvalues <<- t_val
                           p_val = 2*pt(-abs(t_val), df)
                           pvalues <<- p_val
                           
                         },
+                        
+                        quantile_outlier = function(x){
+                          q1 <- quantile(x, 0.25)
+                          q3 <- quantile(x, 0.75)
+                          is_outlier = function(x){
+                            if((x > (q3 + 1.5*(q3-q1))) || (x < (q1 - 1.5*(q3-q1)))) 
+                              return(TRUE)
+                            else 
+                              return(FALSE)
+                          }
+                          return(sapply(residuals, is_outlier))
+                          
+                          },
+                        plot = function(x){ 
+                          p1 <- ggplot(iris, aes(.self$fitted, .self$residuals)) + 
+                            geom_point()+
+                            #geom_point(data = .self$data[.self$outliers,], aes(.self$outliers, 0), shape = 1)+
+                            stat_summary(fun=mean, fun.args = list(trim=0.25), colour="red", geom="line",group=1)+
+                            labs(title = "Residuals vs Fitted", x = "Fitted values", y = "Residuals") +
+                            theme(axis.title.y = element_text(vjust = 0.5, size = 13, face = "bold")) +
+                            theme(axis.title.x = element_text(vjust = 0.5, size = 13, face = "bold")) +
+                            theme(plot.title = element_text(size = 14, face = "bold", hjust = 0.5)) +
+                            theme_bw()
+                          p3 <- ggplot(iris, aes(.self$fitted, sqrt(abs(.self$residuals/sqrt(abs(.self$resvar)))))) + 
+                            geom_point()+
+                            #geom_point(data = .self$data[.self$outliers,], aes(.self$outliers, 0), shape = 1)+
+                            stat_summary(fun=mean, fun.args = list(trim=0.25), colour="red", geom="line",group=1)+
+                            labs(title = "Scale~Location", x = "Fitted values", y = expression(sqrt(Standardized~residuals))) +
+                            theme(axis.title.y = element_text(vjust = 0.5, size = 13, face = "bold")) +
+                            theme(axis.title.x = element_text(vjust = 0.5, size = 13, face = "bold")) +
+                            theme(plot.title = element_text(size = 14, face = "bold", hjust = 0.5)) +
+                            theme_bw() 
+                          return(list(p1,p3))
+                          },
+                        resid = function(){return(.self$residuals)},
+                        pred = function(){return(.self$fitted)},
+                        coef = function(){return(.self$coefficients)},
+                        summary = function(){
+                          cat("Coefficients \n")
+                          print(.self$coefficients)
+                          cat("Standard Errors \n")
+                          print("How do I get them?")
+                          print(cbind(T_values=.self$tvalues, P_values=.self$pvalues))
+                          cat(" Degrees of Freedom \n")
+                          cat(.self$degrees_of_freedom)
+                          },
                         
                         initialize = function(formula, data){
                           call_params <<- as.character(sys.calls()[1])  # returns pairlist of all the active calls
                           formula <<- formula
                           data <<- data
                           .self$fit()
+                          .self$outliers <<- .self$quantile_outlier(.self$residuals)
                         },
                         
                         show = function(){ 
@@ -65,7 +115,6 @@ Linreg <- setRefClass("Linreg",
                           cat('\n')
                           cat("Coefficients: \n")
                           print(t(coefficients))
-                          
                         }
                       )
           )
@@ -73,50 +122,9 @@ Linreg <- setRefClass("Linreg",
 #custom_reg = Linreg(formula = Petal.Length ~ Species, data = iris)
 
 #custom_reg$fit()
-print(custom_reg)
+#print(custom_reg)
 
-library(ggplot2)
+#library(ggplot2)
 
-d <- iris
-fit <- Linreg(formula = Petal.Length ~ Species, data = d)
-
-# ggplot(iris, aes(x = fit$fitted, y = fit$residuals)) + geom_point()
-
-# Another way
-quantile_outlier <- function(d){
-q1 <- quantile(d$residuals, 0.25)
-q3 <- quantile(d$residuals, 0.75) 
-d$outlier <- ifelse(d$residuals > (q3 + 1.5*(q3-q1)), TRUE,
-                 ifelse(d$residuals < (q1 - 1.5*(q3-q1)), 1, 0))
-return(d$outlier)
-}
-
-plot_fun <- function() {
-  p1 <- ggplot(iris, aes(fit$fitted, fit$residuals)) + 
-    geom_point(data = d[quantile_outlier(d$outlier),], aes(outlier, 0), shape = 1) +
-    stat_smooth(method = "loess") + 
-    geom_hline(yintercept = 0, color = "red", linetype = "dashed") +
-    labs(title = "Residuals vs Fitted", x = "Fitted values", y = "Residuals") +
-    theme(axis.title.y = element_text(vjust = 0.5, size = 13, face = "bold")) +
-    theme(axis.title.x = element_text(vjust = 0.5, size = 13, face = "bold")) +
-    theme(plot.title = element_text(size = 14, face = "bold", hjust = 0.5)) +
-    theme_bw() 
-   
-  p3 <- ggplot(iris, aes(fit$fitted, sqrt(abs(fit$resvar)))) + 
-    geom_point() +
-    stat_smooth(method = "loess") + 
-    xlab("Fitted Values") +
-    ylab(expression(sqrt("|Standardized residuals|"))) +
-    ggtitle("Scale-Location") + 
-    theme_bw() +
-    theme(plot.title = element_text(hjust = 0.5, size = 10, face = "bold"))
-  
-  return(list(rvf = p1, fvsd = p3))
-}
-
-plot_fun()
-
-# Comments: So I tried to plot residuals vs. fitted and fitted vs residual variance (scale-location). 
-# The variables should mainly be assigned on ggplot. Everything else is just aeshetics for R. 
-# p1 works after some warnings but I still can't get the outliers in, as showed in the pdf (wrote a function that detects them and tried to implement it within the plot).
-# p3 throws an error
+#d <- iris
+#fit <- Linreg(formula = Petal.Length ~ Species, data = d)
