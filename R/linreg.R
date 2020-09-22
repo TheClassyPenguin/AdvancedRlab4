db92c26b2d17f3ccf71fc883eea6cc353db31534
Eleftheria94 <e.hatzitheodoridi@gmail.com>
2020-09-22 18:18:17 +0200

    Added Roxygen Documentation on linreg function and vignettes. Renamed vignettes file and changes description. Deleted iris.data file

--- R/linreg.R
+++ R/linreg.R
@@ -1,5 +1,42 @@
-data("iris")
-# ' @export
+################################################################################
+#
+#name = "Salvador Marti Roman & Eleftheria Chatzitheodoridou"
+#liuid = "salma742 & elech646"
+#
+# 732A94 Advanced R Programming
+# Computer lab 4#
+#lab_path = "https://raw.githubusercontent.com/STIMALiU/AdvRCourse/master/Labs/Tests/lab4.yml"
+# package.skeleton(name="lab4")
+# Deadline: 22 September 23:59
+################################################################################
+
+
+#' Linreg Class
+#' 
+#' This class performs linear regression and provides different methods to display the data. It also prints the plots for Residual vs. Fitted Values and Scale-Location
+#' 
+#' The algorithm used for this function can be found on 
+#' \url{https://en.wikipedia.org/wiki/Linear_regression}
+#' 
+#' @field balance matrix. Where is balance bruh.
+#' @field coefficients matrix. Regression Coefficients.
+#' @field fitted matrix. Fitted Values.
+#' @field residuals matrix. Residuals.
+#' @field degrees_of_freedom numeric. Degrees of freedom.
+#' @field resvar matrix. Residual Variance.
+#' @field coefficients_variance matrix. Variance of the Regression Coefficients.
+#' @field formula formula. The Formula for the Linear Regression.
+#' @field pvalues vector. P-values.
+#' @field tvalues vector. T-values for each Coefficient.
+#' @field data_name character. The Given Data.
+#' @field data data.frame. The given data frame.
+#' @field call_params character. A pairlist of all active calls.
+#' @field outliers vector. Outliers.
+#' 
+#' @return Nothing.
+#' @export linreg
+#' @exportClass linreg
+
 linreg <- setRefClass("linreg",
                        fields = list(balance = "numeric",
                                      formula = "formula",
@@ -17,6 +54,7 @@ linreg <- setRefClass("linreg",
                                      degrees_of_freedom = "numeric"
                                      ),
                       
+                      # Methods
                       methods = list(
                         fit = function(){
                           
@@ -54,11 +92,11 @@ linreg <- setRefClass("linreg",
                           var_beta_hat = as.numeric(s_hat) * solve(t(X) %*% X) 
                           coefficients_variance <<- var_beta_hat
                           
-                          #T Values
+                          #T-Values
                           t_val = as.vector(beta_hat) / sqrt(pmax(0,var_beta_hat))
                           tvalues <<- t_val
                           
-                          #P Values
+                          #P-Values
                           p_val = 2*pt(-abs(t_val), df)
                           pvalues <<- p_val
                           
@@ -68,6 +106,7 @@ linreg <- setRefClass("linreg",
                           q1 <- quantile(x, 0.25)
                           q3 <- quantile(x, 0.75)
                           is_outlier = function(x){
+                            # Checks for outliers
                             if((x > (q3 + 1.5*(q3-q1))) || (x < (q1 - 1.5*(q3-q1)))) 
                               return(TRUE)
                             else 
@@ -76,7 +115,11 @@ linreg <- setRefClass("linreg",
                           return(sapply(residuals, is_outlier))
                           
                           },
+                        
                         plot = function(x){ 
+                          # Plots Residuals vs. Fitted Values for p1 and Residuals vs. Standardized Residuals for p3
+                          
+                          library(ggplot2)
                           p1 <- ggplot(iris, aes(.self$fitted, .self$residuals)) + 
                             geom_point()+
                             #geom_point(data = .self$data[.self$outliers,], aes(.self$outliers, 0), shape = 1)+
@@ -86,6 +129,7 @@ linreg <- setRefClass("linreg",
                             theme(axis.title.x = element_text(vjust = 0.5, size = 13, face = "bold")) +
                             theme(plot.title = element_text(size = 14, face = "bold", hjust = 0.5)) +
                             theme_bw()
+                          
                           p3 <- ggplot(iris, aes(.self$fitted, sqrt(abs(.self$residuals/sqrt(abs(.self$resvar)))))) + 
                             geom_point()+
                             #geom_point(data = .self$data[.self$outliers,], aes(.self$outliers, 0), shape = 1)+
@@ -97,8 +141,13 @@ linreg <- setRefClass("linreg",
                             theme_bw() 
                           return(list(p1,p3))
                           },
+                        # resid function, returns residuals 
                         resid = function(){return(.self$residuals)},
+                        
+                        # pred function, returns fitted values
                         pred = function(){return(.self$fitted)},
+                        
+                        # coef function, returns coefficients
                         coef = function(){return(.self$coefficients)},
                         
                         summary = function(){
@@ -126,6 +175,8 @@ linreg <- setRefClass("linreg",
                           },
                         
                         initialize = function(formula, data){
+                          # Creating object. Arguments are the formula and corresponding data frame.
+                          
                           stopifnot(inherits(formula,"formula"))
                           stopifnot(inherits(data,"data.frame"))
                           call_params <<- as.character(sys.calls()[1])  # returns pairlist of all the active calls
@@ -153,8 +204,12 @@ linreg <- setRefClass("linreg",
 #print(custom_reg)
 
 #library(ggplot2)
-
 #d <- iris
 #fit <- Linreg(formula = Petal.Length ~ Species, data = d)
 #linreg_mod <- linreg$new(Petal.Length~Sepal.Width+Sepal.Length, data=iris)
 #linreg_mod$print()
+#linreg_mod$summary()
+#linreg_mod$plot()
+#linreg_mod$resid()
+#linreg_mod$pred()
+#linreg_mod$coeff()
